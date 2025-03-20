82281de49570b4b060b1685ae6ccee519a852b06
Mrowr Purr <mrowr.purr@gmail.com>
2025-03-19 20:14:57 -0700

    There we go, nice and simple (only needed for NG)

--- plugin.cpp
+++ plugin.cpp
@@ -1,16 +1,15 @@
 #include <RE/Skyrim.h>
 #include <SKSE/SKSE.h>
+#include <SKSEPluginInfo.h>
+#include <SkyrimScripting/Logging.h>
+
 
 extern "C" __declspec(dllexport) bool SKSEPlugin_Load(const SKSE::LoadInterface* a_skse) {
+    // This line only needed when using NG
+    // Note you can pass the plugin name from someplace else if not using SKSEPluginInfo
+    SkyrimScripting::Logging::InitializeLogger(SKSEPluginInfo::GetPluginName());
+
     SKSE::Init(a_skse);
-
-    SKSE::GetMessagingInterface()->RegisterListener(
-        "SKSE",
-        [](SKSE::MessagingInterface::Message* a_msg) {
-            if (a_msg->type == SKSE::MessagingInterface::kDataLoaded)
-                RE::ConsoleLog::GetSingleton()->Print("'Choose Your Own Adventure' initialized.");
-        }
-    );
-
+    SKSE::log::info("THIS IS info LOG!");
     return true;
 }
--- xmake.lua
+++ xmake.lua
@@ -1,12 +1,21 @@
+add_rules("mode.debug", "mode.release")
+
+set_languages("c++23")
+
+add_repositories("SkyrimScripting https://github.com/SkyrimScripting/Packages.git")
+add_repositories("SkyrimScriptingBeta https://github.com/SkyrimScriptingBeta/Packages.git")
+
+add_requires("SkyrimScripting.Logging", {configs = { commonlib = "skyrim-commonlib-ng" }})
+-- add_requires("SkyrimScripting.Logging", {configs = { commonlib = "skyrim-commonlib-ae" }})
+    
 mod_info = {
-    name = "Hello Plugin",
+    name = "Example using SkyrimScripting.Logging",
     version = "0.0.1",
     author = "Mrowr Purr",
-    email = "mrowr.purr@gmail.com",
-    mod_files = {"Scripts"}
+    email = "mrowr.purr@gmail.com"
 }
 
-skyrim_versions = {"ae", "se", "vr", "ng"}
+skyrim_versions = {"ng"}
+-- skyrim_versions = {"ae"}
 
-includes("papyrus.lua")
 includes("skse.lua")
